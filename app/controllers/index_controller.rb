e715afee357a5f5f1828852b2858c35777741c67
Lazigerbill <chaukbill@gmail.com>
2014-09-24 18:45:01 -0400

    refactored twitter codes

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,14 +1,23 @@
 class IndexController < ApplicationController
 	def index
+
+		get_authorized
+		search_tweets('$AAPL')
+	end
+
+private	
+	def get_authorized
 		@tweets = Tweet.where(:tweet_created_at => 7.day.ago..Time.now).order(:tweet_created_at).reverse_order
 		@client = Twitter::REST::Client.new do |config|
-		  config.consumer_key        = "KWfNGEf1dfGMJZ5qZNOsQJ1ap"
-		  config.consumer_secret     = "YU9EPrGwgdjv1ujt6HVxo7jrMbbgs5gabUeeaXHQF59IwLi9Rf"
-		  config.access_token        = "2748660599-MY1es3v57JkwGFH7GI5e4gxC4xtJVsInggx5ZEB"
-		  config.access_token_secret = "AUG3Mv8VzNAZZotIag0jVzCri7oo5Abiqloeb5k949Dmh"
+		config.consumer_key        = "KWfNGEf1dfGMJZ5qZNOsQJ1ap"
+		config.consumer_secret     = "YU9EPrGwgdjv1ujt6HVxo7jrMbbgs5gabUeeaXHQF59IwLi9Rf"
+		config.access_token        = "2748660599-MY1es3v57JkwGFH7GI5e4gxC4xtJVsInggx5ZEB"
+		config.access_token_secret = "AUG3Mv8VzNAZZotIag0jVzCri7oo5Abiqloeb5k949Dmh"
 		end
-		@client.search("$AAPL ", :result_type => "mixed").take(300).each do |tweet|
-			# tweet_created_at = DateTime.parse(tweet.created_at)
+	end
+
+	def search_tweets(ticker)
+		@client.search((ticker), :result_type => "mixed").take(300).each do |tweet|
 			unless Tweet.exists?(['tweet_created_at = ? AND user_id = ?', tweet.created_at, tweet.user.id])
 				@tweet = Tweet.new
 				@tweet.update_attributes(
@@ -22,7 +31,4 @@ class IndexController < ApplicationController
 			end
 		end
 	end
-
-
-
 end
