7754387180a7581ba09b4fa47c6ba723aec11884
Lazigerbill <chaukbill@gmail.com>
2014-10-10 15:06:17 -0400

    figaro installed and working fine

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -24,10 +24,10 @@ class Tweet < ActiveRecord::Base
 
 	def self.get_authorized		
 		Twitter::REST::Client.new do |config|
-			config.consumer_key        = Rails.application.secrets.twitter_consumer_key
-			config.consumer_secret     = Rails.application.secrets.twitter_consumer_secret
-			config.access_token        = Rails.application.secrets.twitter_access_token
-			config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+			config.consumer_key        = Figaro.env.twitter_consumer_key
+			config.consumer_secret     = Figaro.env.twitter_consumer_secret
+			config.access_token        = Figaro.env.twitter_access_token
+			config.access_token_secret = Figaro.env.twitter_access_token_secret
 		end
 	end
 
